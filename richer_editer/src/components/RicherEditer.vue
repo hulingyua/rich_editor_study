84da489e9534255edd65cac24b49b6d006ece0b0
hulingyu <hulingyu@keking.cn>
2018-11-20 14:53:02 +0800

    vue-quill-editor的大致使用和获取富文本编辑器的内容

---
+++ richer_editer/src/components/RicherEditer.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <quill-editor v-model="content"
+            ref="myQuillEditor"
+            :options="editorOption"
+            @blur="onEditorBlur($event)"
+            @change="onEditorChange($event)">
+        </quill-editor>
+        <button @click="saveContent">保存</button>
+    </div>
+
+</template>
+<script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
+import Quill from 'quill'
+// import { someModule } from '../yourModulePath/someQuillModule.js'
+// Quill.register('modules/someModule', someModule)
+
+export default {
+data () {
+    return {
+        content: '<h2>I am Example</h2>',
+        editorOption: {
+            // some quill options
+        }
+    }
+},
+methods: {
+    onEditorBlur(quill) {
+        
+    },
+    onEditorChange({ quill, html, text }) {
+        console.log(html)
+        this.content = html
+        console.log(this.content)
+    },
+    saveContent() {
+        console.log(this.content)
+    }
+},
+computed: {
+    editor() {
+    return this.$refs.myQuillEditor.quill
+    }
+},
+mounted() {
+    console.log('this is current quill instance object', this.editor)
+}
+}
+
+</script>
+<style scoped>
+
+</style>
---
+++ richer_editer/src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// 富文本编辑器插件
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+Vue.use(VueQuillEditor)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
